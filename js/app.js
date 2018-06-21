4b4b5b01894d4d6a0cb0de9bd6fda4c6e435604d
debimsantos <debimortola@gmail.com>
2018-06-21 12:47:08 -0700

    Collision detection added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+let collided = false;
+
 // Enemies our player must avoid
 var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
@@ -22,6 +24,7 @@ Enemy.prototype.update = function(dt) {
     } else {
         this.x = -80;
     }
+    detectCollision();
 };
 
 // Draw the enemy on the screen, required method for game
@@ -37,7 +40,8 @@ var Player = function(x, y) {
 };
 
 // This class requires an update(), render() and
-//  canvas.width = 505; canvas.height = 606;
+// canvas.width = 505; canvas.height = 606;
+// This keeps player inside the canvas
 Player.prototype.update = function() {
     if (this.x > 400) {
       this.x = 400;
@@ -50,31 +54,58 @@ Player.prototype.update = function() {
     }
     if (this.y < 0) {
       this.y = 0;
-      alert('Congratulations! You beat the bugs')
     }
-
-
-//    detectCollision();    TODO
-//    checkWin(); TODO
-
 };
+
+// Source: https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
+// bug size: 101 x 77
+function detectCollision() {
+
+  if (collided) {
+    return;
+  }
+
+  for (let bug of allEnemies) {
+      let bugX = bug.x;
+      let bugY = bug.y;
+
+      if (
+        (player.x < bugX + 50) &&
+        (player.x + 35 > bugX) &&
+        (player.y < bugY + 25) &&
+        (30 + player.y > bugY)
+      ) {
+        alert ('A bug beat you. Play again');
+        collided = true;
+        break;
+      }
+  }
+
+  return collided;
+}
+
+
+//    checkWin(); TODO
+// initiateGame(): TODO
 
 // a handleInput() method.
 Player.prototype.handleInput = function(keys) {
+
     if (keys == 'right') {
-        player.x += 50;
-    }
+        player.x += 50; //player moves right
 
-    if (keys == 'left') {
-        player.x -= 50;
-    }
+    } else if (keys == 'left') {
+        player.x -= 50; //player moves left
 
-    if (keys == 'up') {
-        player.y -= 80;
-    }
+    } else if (keys == 'up') {
+        player.y -= 80; //player moves up
 
-    if (keys == 'down') {
-        player.y += 80;
+        if (player.y < 5) {
+          alert('Congratulations! You beat the bugs')
+        }
+
+    } else if (keys == 'down') {
+        player.y += 80; //player moves down
     }
 };
 
@@ -95,11 +126,6 @@ var allEnemies = [
 ];
 // Place the player object in a variable called player
 var player = new Player(200, 400);
-
-//Player wins if y = 0 TODO
-// bug size: 101 x 171
-
-
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
