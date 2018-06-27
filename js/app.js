fe5ab06a8ea70dc15d3a2b661f7f0b2982949edc
debimsantos <debimortola@gmail.com>
2018-06-27 13:37:54 -0700

    Converted my objects to class

--- js/app.js
+++ js/app.js
@@ -1,125 +1,126 @@
+class Entities {
+    constructor(x, y, speed = 50, sprite) {
+      this.x = x;
+      this.y = y;
+      this.speed = speed;
+      this.sprite = sprite;
+    }
+    // Draw the enemy on the screen, required method for game
+    render(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
+
 let collided = false;
 
 // Enemies our player must avoid
-var Enemy = function(x, y, speed) {
+class Enemy extends Entities {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-    this.x = x;
-    this.y = y;
-    this.speed = Math.random() * speed;
+    constructor(x, y, speed, sprite){
+        super(x, y);
+        this.speed = Math.random() * speed;
 
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+        // The image/sprite for our enemies, this uses
+        // a helper we've provided to easily load images
+        this.sprite = 'images/enemy-bug.png';
+        }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-// You should multiply any movement by the dt parameter
-// which will ensure the game runs at the same speed for
-// all computers.
-Enemy.prototype.update = function(dt) {
-    if (this.x < 505) {
-        this.x += this.speed * dt;
-    } else {
-        this.x = -80;
+    update(dt){
+      // Update the enemy's position, required method for game
+      // Parameter: dt, a time delta between ticks
+      // You should multiply any movement by the dt parameter
+      // which will ensure the game runs at the same speed for
+      // all computers.
+      if (this.x < 505) {
+          this.x += this.speed * dt;
+      } else {
+          this.x = -80;
+      }
     }
-    detectCollision();
-};
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Now write your own player class
-var Player = function(x, y) {
-    this.x = x;
-    this.y = y;
-    this.sprite = 'images/char-catgirl.png';
-};
-
-// This class requires an update(), render() and
-// canvas.width = 505; canvas.height = 606;
-// This keeps player inside the canvas
-Player.prototype.update = function() {
-    if (this.x > 400) {
-      this.x = 400;
-    }
-    if (this.y > 400) {
-      this.y = 400;
-    }
-    if (this.x < 0) {
-      this.x = 0;
-    }
-    if (this.y < 0) {
-      this.y = 0;
-    }
-};
-
-// Source: https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
-// bug size: 101 x 77
-function detectCollision() {
-
-  if (collided) {
-    return;
-  }
-
-  for (let bug of allEnemies) {
-      let bugX = bug.x;
-      let bugY = bug.y;
-
-      if (
-        (player.x < bugX + 50) &&
-        (player.x + 35 > bugX) &&
-        (player.y < bugY + 25) &&
-        (30 + player.y > bugY)
-      ) {
-        setTimeout(function() {
-          alert ('A bug beat you. Play again');
-          }, 500);
-          collided = true;
-          break;
+class Player extends Entities{
+    constructor(x, y, sprite){
+        super (x, y);
+        this.sprite = 'images/char-catgirl.png';
+      }
+    // This class requires an update(), render() and
+    // canvas.width = 505; canvas.height = 606;
+    // This keeps player inside the canvas
+    update(){
+        if (this.x > 400) {
+          this.x = 400;
         }
-  }
+        if (this.y > 400) {
+          this.y = 400;
+        }
+        if (this.x < 0) {
+          this.x = 0;
+        }
+        if (this.y < 0) {
+          this.y = 0;
+        }
+      }
+    // Collision Algo Source: https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
+    // bug size: 101 x 77
+    detectCollision(){
+        if (collided) {
+          return;
+        }
 
-  return collided;
+        for (let bug of allEnemies) {
+            let bugX = bug.x;
+            let bugY = bug.y;
+
+            if (
+              (player.x < bugX + 50) &&
+              (player.x + 35 > bugX) &&
+              (player.y < bugY + 25) &&
+              (30 + player.y > bugY)
+            ) {
+              setTimeout(function() {
+                alert ('A bug beat you. Play again');
+                }, 500);
+                collided = true;
+                break;
+              }
+        }
+        return collided;
+    }
+
+    // Key press for left, right, up, down movement
+    handleInput(keys) {
+
+        if (keys == 'right') {
+            player.x += 50; //player moves right
+
+        } else if (keys == 'left') {
+            player.x -= 50; //player moves left
+
+        } else if (keys == 'up') {
+            player.y -= 80; //player moves up
+
+            if (player.y < 5) {  //player reaches water
+              setTimeout(function() {
+              alert('Congratulations! You beat the bugs')
+              }, 500);
+            }
+
+        } else if (keys == 'down') {
+            player.y += 80; //player moves down
+        }
+}
 }
 
 
-//    checkWin(); TODO
+// Reset function when player hits play again? TODO
 // initiateGame(): TODO
-
-// a handleInput() method.
-Player.prototype.handleInput = function(keys) {
-
-    if (keys == 'right') {
-        player.x += 50; //player moves right
-
-    } else if (keys == 'left') {
-        player.x -= 50; //player moves left
-
-    } else if (keys == 'up') {
-        player.y -= 80; //player moves up
-
-        if (player.y < 5) {
-          setTimeout(function() {
-          alert('Congratulations! You beat the bugs')
-          }, 500);
-        }
-
-    } else if (keys == 'down') {
-        player.y += 80; //player moves down
-    }
-};
-
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-var allEnemies = [
+let allEnemies = [
     new Enemy (-20, 65, 300),
     new Enemy (-120, 65, 100),
     new Enemy (-50, 145, 250),
@@ -129,7 +130,7 @@ var allEnemies = [
 
 ];
 // Place the player object in a variable called player
-var player = new Player(200, 400);
+let player = new Player(200, 410);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
